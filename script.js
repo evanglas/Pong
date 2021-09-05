ad63be666461e484ddf9d854c11a9deda7db44d5
Evan Glas <43274966+eglas-20@users.noreply.github.com>
2021-09-04 22:25:57 -0400

    Moved red square

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var pongBall;
 
+var myGameArea;
+
 function loadGame() {
-    myGameArea.launch();
     pongBall = new rectangle(30, 30, "red", 20, 120);
+    myGameArea.launch();
 }
 
 var myGameArea = {
@@ -12,6 +14,10 @@ var myGameArea = {
         this.canvas.height = 400;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
@@ -20,17 +26,31 @@ function rectangle(width, height, color, x, y) {
     this.height = height;
     this.x = x;
     this.y = y;
-    ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    this.vx = 0;
+    this.vy = 0;
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.vx;
+        this.y += this.vy;
+    }
 }
 
 function moveUp() {
-    pongBall.x = pongBall.x + 10;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-
+    myGamePiece.speedY -= 1;
 }
 
+// function moveUp() {
+//     pongBall.x = pongBall.x + 10;
+//     ctx.fillRect(this.x, this.y, this.width, this.height);
+
+// }
+
 function updateGameArea() {
-    
+    myGameArea.clear();
+    pongBall.x += 1;
+    pongBall.update();
 }
