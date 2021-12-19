d1821329f6ba24f63f405e31e131a5fb40dc55f5
Evan Glas <43274966+eglas-20@users.noreply.github.com>
2021-12-19 16:42:57 -0500

    Basic 2 player working, 1 person + simple AI

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ var pongBall;
 
 var myGameArea;
 
-let canvasWidth = 600;
-let canvasHeight = 400;
-let pongBallWidth = 50;
-let pongBallHeight = 50;
+let canvasWidth = document.body.clientWidth;
+let canvasHeight = window.innerHeight;
+let pongBallWidth = 30;
+let pongBallHeight = 30;
 let paddleHeight = 90;
 let paddleWidth = 10;
 
 function loadGame() {
-    pongBall = new rectangle(pongBallHeight, pongBallWidth, "red", 300, 120, "ball");
-    paddle1 = new rectangle(paddleWidth, paddleHeight, "black", 10, 100, "paddle");
+    pongBall = new rectangle(pongBallHeight, pongBallWidth, "white", canvasWidth / 2, canvasHeight / 2 - pongBallHeight / 2, "ball");
+    paddle1 = new rectangle(paddleWidth, paddleHeight, "white", 10, canvasHeight / 2 - paddleHeight / 2, "paddle");
+    paddle2 = new rectangle(paddleWidth, paddleHeight, "white", canvasWidth - 10 - paddleWidth, canvasHeight / 2 - paddleHeight / 2, "paddle");
+    midLine = new rectangle(2, canvasHeight, "white", canvasWidth / 2, 0, "line");
     myGameArea.launch();
 }
 
@@ -35,6 +37,9 @@ var myGameArea = {
     },
     clear: function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop: function() {
+        clearInterval(this.interval);
     }
 }
 
@@ -48,7 +53,7 @@ function rectangle(width, height, color, x, y, type) {
     this.angle = 0;
     this.angularVelo = 0;
     if (type == "ball") {
-        this.vx = -3;
+        this.vx = -10;
     }
     this.update = function() {
         context = myGameArea.context;
@@ -74,7 +79,16 @@ function rectangle(width, height, color, x, y, type) {
         this.y += this.vy;
     }
     this.newAngle = function() {
-        this.angle = this.angle + this.angularVelo;
+        this.angle = this.angle + 3 * this.angularVelo;
+    }
+}
+
+function score(width, height, color, x, y) {
+    this.update() = function() {
+        context = myGameArea.context;
+        context.font = width + " " + height;
+        context.fillStyle = color;
+        context.fillTest()
     }
 }
 
@@ -92,33 +106,63 @@ function updateGameArea() {
     myGameArea.clear();
     paddle1.vx = 0;
     paddle1.vy = 0;
-    if (myGameArea.keys && myGameArea.keys[37]) {pongBall.vx = -1; }
-    if (myGameArea.keys && myGameArea.keys[39]) {pongBall.vx = 1; }
 
+    paddle2.vx = 0;
+    paddle2.vy = 0;
 
-    if (myGameArea.keys && myGameArea.keys[38]) {paddle1.vy = -5; }
-    if (myGameArea.keys && myGameArea.keys[40]) {paddle1.vy = 5; }
+    if (myGameArea.keys && myGameArea.keys[39]) {paddle2.vx = 3; }
+    if (myGameArea.keys && myGameArea.keys[37]) {paddle2.vx = -3; }
+    if (myGameArea.keys && myGameArea.keys[38]) {paddle2.vy = -5; }
+    if (myGameArea.keys && myGameArea.keys[40]) {paddle2.vy = 5; }
 
-    if (pongBall.x < 20) {
+    if (myGameArea.keys && myGameArea.keys[65]) {paddle1.vx = -3; }
+    if (myGameArea.keys && myGameArea.keys[68]) {paddle1.vx = 3; }
+    if (myGameArea.keys && myGameArea.keys[87]) {paddle1.vy = -5; }
+    if (myGameArea.keys && myGameArea.keys[83]) {paddle1.vy = 5; }
+
+    if (pongBall.x + pongBallWidth > canvasWidth) {
+        // myGameArea.stop();
+        pongBall.vx = -pongBall.vx;
+    } else if (pongBall.x < 0) {
+        myGameArea.stop();
+    } else if ((pongBall.x <= paddle1.x + paddle1.width) && (pongBall.x > paddle1.x - paddle1.width)) {
         let hitPaddle1 = false;
         if ((pongBall.y + pongBallHeight) > paddle1.y) {
             if ((pongBall.y + pongBallHeight / 2) < paddle1.y + paddleHeight / 3) {
                 hitPaddle1 = true;
-                pongBall.vy = -1;
+                pongBall.vy = pongBall.vy - 1;
             } else if ((pongBall.y + pongBallHeight / 2) < paddle1.y + 2 * paddleHeight / 3) {
                 hitPaddle1 = true;
-                pongBall.vy = 0;
+                pongBall.vy = pongBall.vy;
             } else if (pongBall.y < paddle1.y + paddleHeight) {
                 hitPaddle1 = true;
-                pongBall.vy = 1;
+                pongBall.vy = pongBall.vy + 1;
             }
         }
         if (hitPaddle1) {
             pongBall.vx = -pongBall.vx;
+            paddle1.vx = -3;
             pongBall.angularVelo = pongBall.angularVelo - paddle1.vy * Math.PI / 360;
         }
-    } else if (pongBall.x + pongBallWidth > canvasWidth) {
-        pongBall.vx = -pongBall.vx;
+    } else if ((pongBall.x + pongBallWidth >= paddle2.x) && (pongBall.x < paddle2.x + paddle2.width)) {
+        let hitPaddle2 = false;
+        if ((pongBall.y + pongBallHeight) > paddle2.y) {
+            if ((pongBall.y + pongBallHeight / 2) < paddle2.y + paddleHeight / 3) {
+                hitPaddle2 = true;
+                pongBall.vy = pongBall.vy - 1;
+            } else if ((pongBall.y + pongBallHeight / 2) < paddle2.y + 2 * paddleHeight / 3) {
+                hitPaddle2 = true;
+                pongBall.vy = pongBall.vy;
+            } else if (pongBall.y < paddle2.y + paddleHeight) {
+                hitPaddle2 = true;
+                pongBall.vy = pongBall.vy + 1;
+            }
+        }
+        if (hitPaddle2) {
+            pongBall.vx = -pongBall.vx;
+            paddle2.vx = 3;
+            pongBall.angularVelo = pongBall.angularVelo + paddle2.vy * Math.PI / 360;
+        }
     }
 
     if (pongBall.y < 0) {
@@ -127,9 +171,14 @@ function updateGameArea() {
         pongBall.vy = -pongBall.vy;
     }
 
+    // paddle2.vy = 2 * Math.sign((pongBall.y + pongBallHeight / 2) - (paddle2.y + paddleHeight / 2));
+
     pongBall.newAngle();
     pongBall.newPos();    
     pongBall.update();
     paddle1.newPos();
     paddle1.update();
+    paddle2.newPos();
+    paddle2.update();
+    midLine.update();
 }
